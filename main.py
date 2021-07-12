dc1ef2eb79cd5498d4a1d521b25bae22cf302fb6
Saravanan Elumalai <saravanan.elumalai0@walmart.com>
2021-07-12 17:09:23 +0530

    Fixed to run with boundaries

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ webbrowser.open("web.whatsapp.com")
 time.sleep(30)
 count=1
 while count < 5:
-    pyautogui.press("R")
+    pyautogui.press("Trying Something")
     pyautogui.press("enter")
     count=count+1
+    time.sleep(1)
